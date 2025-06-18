e21559639961fa9e74185842049773b707d6b7d2
Dimas Agil S. W. <92960385+projectdasw@users.noreply.github.com>
2025-06-18 16:36:24 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/', 'Auth::login');
+$routes->get('/login', 'Auth::login');
+$routes->post('/login-process', 'Auth::loginProcess');
+$routes->get('/logout', 'Auth::logout');
+$routes->get('/dashboard', 'Dashboard::index');
+$routes->get('/test-db', 'TestDB::index');
